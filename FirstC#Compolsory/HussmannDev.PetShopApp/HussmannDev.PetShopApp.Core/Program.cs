d25f21dd6192de32300484d9c449e9ebef51f7dc
christianhussmann <54892854+christianhussmann@users.noreply.github.com>
2021-08-25 09:34:17 +0200

    PetShop start

---
+++ FirstC#Compolsory/HussmannDev.PetShopApp/HussmannDev.PetShopApp.Core/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HussmannDev.PetShopApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
