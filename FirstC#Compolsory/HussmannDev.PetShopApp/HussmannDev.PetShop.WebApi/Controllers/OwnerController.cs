07fb98427c3f957b3cb358886d53423e39f0730c
Christian Hussmann <chri.hussmann@gmail.com>
2021-09-08 11:46:07 +0200

    Update OwnerController.cs

--- FirstC#Compolsory/HussmannDev.PetShopApp/HussmannDev.PetShop.WebApi/Controllers/OwnerController.cs
+++ FirstC#Compolsory/HussmannDev.PetShopApp/HussmannDev.PetShop.WebApi/Controllers/OwnerController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using HussmannDev.PetShopApp.Core.IServices;
+using HussmannDev.PetShopApp.Core.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -13,5 +14,40 @@ namespace HussmannDev.PetShop.WebApi.Controllers
     public class OwnerController : ControllerBase
     {
         private readonly IOwnerService _ownerService;
+
+        public OwnerController(IOwnerService ownerService)
+        {
+            _ownerService = ownerService;
+        }
+
+        [HttpPost]
+        public Owner Create(Owner owner)
+        {
+            return _ownerService.CreateOwner(owner);
+        }
+
+        [HttpGet]
+        public List<Owner> GetAll()
+        {
+            return _ownerService.ReadAllOwners();
+        }
+
+        [HttpDelete("{id}")]
+        public Owner Delete(int id)
+        {
+            return _ownerService.RemoveOwner(id);
+        }
+
+        [HttpGet("{id}")]
+        public Owner ReadById(int id)
+        {
+            return _ownerService.ReadOwnerById(id);
+        }
+
+        [HttpPut("{id}")]
+        public Owner Edit(Owner owner)
+        {
+            return _ownerService.UpdateOwner(owner);
+        }
     }
 }
